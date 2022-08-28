187f7f05d43c79584acc768d913ad8e7ace9c3cd
Jesus Bolivar <j5rojo94@gmail.com>
2022-08-28 02:48:04 -0400

    Clases CSS para tipografia en Quasar

--- src/pages/TyphographyPage.vue
+++ src/pages/TyphographyPage.vue
@@ -1,7 +1,22 @@
 <template>
-    <q-page class="flex">
+    <q-page class="flex column q-pa-md">
 
-        <h1>Typhography</h1>
+        <span class="text-h1">Typhography</span>
+        <span class="text-h2">Typhography</span>
+        <span class="text-h3">Typhography</span>
+        <span class="text-h4">Typhography</span>
+        <span class="text-h5">Typhography</span>
+        <span class="text-h6">Typhography</span>
+
+        <q-separator/>
+
+        <span class="text-subtitle1">Subtitule 1</span>
+        <span class="text-subtitle2">Subtitule 2</span>
+
+        <q-separator/>
+
+        <p class="text-body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, magni.</p>
+        <p class="text-body2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, magni!</p>
 
     </q-page>
 </template>
