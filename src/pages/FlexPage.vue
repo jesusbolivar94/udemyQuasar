cb67b7acc0f64dc668a9949d26b74018c1ba7981
Jesus Bolivar <j5rojo94@gmail.com>
2022-08-30 01:16:53 -0400

    Clases flexbox en quasar

--- src/pages/FlexPage.vue
+++ src/pages/FlexPage.vue
@@ -1,7 +1,46 @@
 <template>
-    <q-page class="flex">
+    <q-page>
+        <div class="row q-mb-md">
+            <div class="col bg-primary"></div>
+            <div class="col bg-secondary"></div>
+            <div class="col bg-accent"></div>
+        </div>
+        <div class="row q-mb-md">
+            <div class="col bg-primary"></div>
+            <div class="col-6 bg-secondary"></div>
+            <div class="col bg-accent"></div>
+        </div>
+        <div class="row q-mb-md justify-center">
+            <div class="col-4 bg-primary"></div>
+            <div class="col-4 bg-secondary"></div>
+        </div>
+        <div class="row q-mb-md justify-end">
+            <div class="col-4 bg-primary"></div>
+            <div class="col-4 bg-secondary"></div>
+        </div>
+        <div class="row q-mb-md justify-between">
+            <div class="col-4 bg-primary"></div>
+            <div class="col-4 bg-secondary"></div>
+        </div>
 
-        <h1>Flex.vue</h1>
+        <q-separator spaced/>
+
+        <div class="row q-mb-md justify-between">
+            <div class="col-4 bg-primary"></div>
+            <div class="col-4 bg-secondary"></div>
+        </div>
+        <div class="row q-mb-md">
+            <div class="col-12 col-md-6 bg-primary"></div>
+            <div class="col-12 col-md-6 bg-secondary"></div>
+        </div>
+
+        <q-separator spaced/>
+
+        <div class="row justify-between">
+            <div class="square-box q-mx-md bg-primary"></div>
+            <div class="square-box q-mx-md bg-secondary"></div>
+            <div class="square-box q-mx-md bg-accent"></div>
+        </div>
 
     </q-page>
 </template>
@@ -13,3 +52,13 @@ export default defineComponent({
     name: "FlexPage"
 });
 </script>
+
+<style scoped>
+.col, .col-4, .col-6, .col-12 {
+    height: 50px;
+}
+.square-box {
+    width: 50px;
+    height: 50px;
+}
+</style>
